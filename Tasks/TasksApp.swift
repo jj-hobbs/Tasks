f036906fbc1a32115ee105faa44d7815c8856098
justin.hobbs <justinhobbs@fastmail.com>
2023-01-04 18:19:03 -0600

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Justin Hobbs on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
